599eeae0c2de181788ca8e04e558ee87b89ae581
Ashley Boers <ashley.boers@gmail.com>
2022-09-30 12:04:55 +1000

    Add files via upload

---
+++ serial-debug.ino
@@ -0,0 +1,21 @@
+// Simple sketch to turn an Arduino-compatible device into a
+//  USB-to-serial bridge.
+
+// Note that there is no need to include *any* Serial functions
+//  *at all*. Doing so would only serve to override the
+//  pinMode() commands. The baud rate will be set by the
+//  computer when the serial program connects to the bridge
+//  device.
+
+void setup()
+{
+  // Make the RX/TX lines inputs, so they don't contend with
+  //  the lines on the pcDuino and cause data issues or damage.
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
+}
+
+void loop()
+{
+  // Nothing here, because we don't *need* anything here.
+}
